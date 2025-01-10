9adb56086f50d301b9b065f3c23fe006ea6f3954
Elena Vitsiou <150396300+ElenaVts@users.noreply.github.com>
2025-01-11 00:10:30 +0200

    Update work.c

--- Phase One/work.c
+++ Phase One/work.c
@@ -1,8 +1,8 @@
 //Code goes here
 ../work/work7 1
 ../work/work6 1
-../work/work5 2
+../work/work5 1
 ../work/work4 1
-../work/work3 2
-../work/work2 3
-../work/work1 3
+../work/work3 1
+../work/work2 1
+../work/work1 1
